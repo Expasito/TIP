d7cae371775a4a0f0ba1b28330343b1715789b76
Expasito <93100379+Expasito@users.noreply.github.com>
2021-10-24 22:09:46 -0700

    Create routes.py

---
+++ root/routes.py
@@ -0,0 +1,17 @@
+from root import app
+from flask import render_template
+@app.route('/')
+@app.route('/home')
+def home():
+
+    return render_template('home.html',name="Expasito")
+
+@app.route('/fileone')
+def fileone():
+    test_users=("Expasito","Sam","George","Steve")
+    return render_template('pageone.html',users=test_users,num=len(test_users))
+
+@app.route('/filetwo')
+def filetwo():
+    test_posts=("Hello","Hi","TestOne","TestTwo")
+    return render_template('pagetwo.html', posts=test_posts)
